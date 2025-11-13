68ee3a9a9d343f35c5fa48a0422ab1a7051dc8c0
tammosminia <tammosminia@gmail.com>
2025-11-13 10:38:02 +0100

    typeclass

--- src/Safelog.hs
+++ src/Safelog.hs
@@ -3,16 +3,20 @@ module Safelog where
 import Data.List
 import qualified Data.Map as Map
 
+-- Define the typeclass
 class Safelog a where
   logSafe :: a -> String
 
+-- Use the typeclass
 logInfo :: Safelog a => a -> IO ()
 logInfo line = print (logSafe line)
 
+-- Example domain
 data UserId = UserId String
 data Email = Email String
 data User = User { id :: UserId, email:: Email }
 
+-- Instances of the typeclass
 instance Safelog UserId where
   logSafe (UserId id) = "userId " ++ id
 
